68ca8fb5cc86046b00ee247c4dde99174fe7c460
Andre Parron <andreparoon@gmail.com>
2024-05-26 14:32:36 -0300

    Criando e estilizando home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 
 import Head from "next/head";
 
+import styles from './home.module.scss'
+import { SubscribeButton } from "../components/SubscribeButton";
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | 01.news</title>
+        <title>Home | 01.news</title>
       </Head>
-      <h1 >Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, Welcome!</span>
+    <h1>News about the <span>React</span> world.</h1>
+      <p>
+        Get acess to all the publications <br />
+        <span>for $9.90 month</span>
+      </p>
+      <SubscribeButton/>
+      </section>
+      <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
